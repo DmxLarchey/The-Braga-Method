3f9190410c629b3d95bc21dfbe893006868e4a76
DmxLarchey <larchey@gmail.com>
2024-02-01 11:38:27 +0100

    cleanup of the OCaml companion code that now decorates a tree with its height and order in breadth first traversal order

--- theories/tree_height_via_bfs/height_via_bfs.ml
+++ theories/tree_height_via_bfs/height_via_bfs.ml
@@ -1,57 +1,45 @@
+type 'a rtree = Rt of 'a * ('a rtree list);;
 
-let t1 = Tree (1,[Tree (2,[Tree (4,[]);Tree (5,[])]);Tree (3,[])]);;
+let t1 = Rt (1,[Rt (2,[Rt (4,[]);Rt (5,[])]);Rt (3,[])]);;
 
-let rec bfs a = function 
-| []            -> List.rev a
-| Tree (i,l)::m -> bfs (i::a) (m@l);;
+let leaf x = Rt (x,[]);;
+let left x t = Rt (x,[t;leaf 0]);;
+let right x t = Rt (x,[leaf 0;t]);;
 
-bfs [] [t1];;
-
-type 'a tree = Tree of 'a * ('a tree list);;
-
-let ht t =
-  let rec loop n = function
-  | []       -> n
-  | None::[] -> n
-  | None::m  -> loop (1+n) (m @ [None])
-  | Some (Tree (i,l))::m 
-             -> loop n (m @ List.map (fun t -> Some t) l)
-  in loop 0 [None;Some t];;
-
-(* Using JC idea of replacing None/Some with
-   two list, n(ext) level and c(urrent level,
-   and using the fact that the height does not
-   depend on the order between the siblings,
-   we can avoid external functions. The termination
-   could certainly be measure based. *)
-
-let rec rev_app a = function
-| []   -> a
-| x::l -> rev_app (x::a) l;;
-
-let ht t =
-  let rec level h n = function
-  | []            -> next h n 
-  | Tree (_,l)::c -> level h (rev_app n l) c
-  and next h n = match n with
-  | [] -> h 
-  | _  -> level (1+h) [] n
-  in level 1 [] [t];;
-
-let leaf = Tree (0,[]);;
-let left t = Tree (0,[t;leaf]);;
-let right t = Tree (0,[leaf;t]);;
-
-let tree_of_ht =
-  let rec loop b n =
-    if n = 1 then leaf
-    else (if b then left else right) (loop (not b) (n-1)) 
-  in loop true;;
+let rtree_of_ht i =
+  let rec loop i b n =
+    if n = 1 then leaf i
+    else (if b then left i else right i) (loop (1+i) (not b) (n-1)) 
+  in loop i true i;;
 
 let rec list_an a n =
   if n = 0 then []
   else a::list_an (a+1) (n-1);;
 
-let t n = Tree (0,List.map tree_of_ht (list_an 1 (n-1)));;
+let t n = Rt (0,List.map rtree_of_ht (list_an 1 (n-1)));;
 
-ht (tree_of_ht 200);;
+let rec rev_app a = function
+| []   -> a
+| x::l -> rev_app (x::a) l;;
+
+let rev_map f = 
+  let rec loop a = function
+  | []   -> a
+  | x::l -> loop (f x::a) l
+  in loop;;
+
+(** A recursive terminal BFS algorithm marking the nodes with
+    a pair composed of its height and its order in BFS traversal
+    order *)
+let bfs t =
+  let rec level h i a n = function
+  | []          -> next (1+h) i a (rev_app [] n)
+  | Rt (x,l)::c -> level h (1+i) ((x,(h,i))::a) (rev_app n l) c
+  and next h i a n = match n with
+  | [] -> rev_app [] a
+  | _  -> level h i a [] n
+  in level 0 0 [] [] [t];;
+
+let t2 = t 3;;
+bfs t2;;
+
