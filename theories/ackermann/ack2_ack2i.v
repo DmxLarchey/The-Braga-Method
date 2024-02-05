335f6e9b8cc19bee1834afe089c39b9114b0eeff
DmxLarchey <larchey@gmail.com>
2024-02-05 05:31:24 +0100

    retravail de la version de David

---
+++ theories/ackermann/ack2_ack2i.v
@@ -0,0 +1,88 @@
+(** DLW: ceci est un retravail d'un code initial de David Monniaux *) 
+
+(* Unset Elimination Schemes. *)
+
+(* On garde la forme existencielle ici, au lieu de déplier et on 
+   a donc un type mutuellement inductif imbriqué avec ex(ists) et
+   and ("et" logique) *)
+Inductive ack2_spec : nat -> nat -> nat -> Prop :=
+| ack2_m0 : forall n, ack2_spec O n (S n)
+| ack2_mS : forall m n y, (exists x, ack2i_spec m n x /\ ack2_spec m x y) -> ack2_spec (S m) n y
+
+with ack2i_spec : nat -> nat -> nat -> Prop :=
+| ack2i_n0 : forall m, ack2i_spec m O (S O)
+| ack2i_nS : forall m n x, ack2_spec (S m) n x -> ack2i_spec m (S n) x.
+
+Lemma ack2_spec_inv {m n y} :
+    ack2_spec m n y ->
+      match m with
+      | O => y = (S n)
+      | S m => exists x, ack2i_spec m n x /\ ack2_spec m x y
+      end.
+Proof. now intros []. Qed.
+
+Definition not0 n := match n with 0 => False | _ => True end.
+
+Definition ack2_spec_inv_S {m n y} (a : ack2_spec (S m) n y) : exists x, ack2i_spec m n x /\ ack2_spec m x y :=
+  match a in ack2_spec m' _ _ return not0 m' -> exists x, ack2i_spec (pred m') _ x /\ ack2_spec (pred m') x _ with
+  | ack2_m0 _       => fun C => match C with end
+  | ack2_mS _ _ _ h => fun _ => h
+  end I.
+
+Lemma ack2i_spec_inv {m n x} :
+    ack2i_spec m n x ->
+    match n with
+    | O => x = (S O)
+    | S n => ack2_spec (S m) n x
+    end.
+Proof. now intros []. Qed.
+
+(* L'inversion ack2i_spec m (S n) x produit une simple valeur
+   donc le code de cette factorisation est bcp plus naturel que
+   ack_spec_inv ci-dessus. *)
+Definition ack2i_spec_inv_S {m n x} (a : ack2i_spec m (S n) x) : ack2_spec (S m) n x :=
+  match a in ack2i_spec _ n' _ return not0 n' -> ack2_spec _ (pred n') _ with
+  | ack2i_n0 _       => fun C => match C with end
+  | ack2i_nS _ _ _ h => fun _ => h
+  end I.
+
+(* La même idée de départ que DM:
+
+     induction mutuelle structurelle sur SPEC1 (resp. SPECi1)
+
+   mais on commence par un match sur m (resp. n) aussi lieu 
+   d'un match sur SPEC1.
+
+   On pourrait d'ailleurs faire ça aussi dans ce cas parce qu'on
+   est dans Prop, càd utiliser le récurseur standard sur les
+   inductif mutuel. C'est plus simple en fait, mais là ce n'est 
+   plus les petites inversions *)
+Fixpoint ack2_spec_det {m n r1 r2} (SPEC1 : ack2_spec m n r1) (SPEC2 : ack2_spec m n r2) {struct SPEC1 } : r1 = r2
+   with  ack2i_spec_det {m n r1 r2} (SPECi1 : ack2i_spec m n r1) (SPECi2 : ack2i_spec m n r2) {struct SPECi1} : r1 = r2.
+Proof.
+  + destruct m as [ | m ].
+    * apply ack2_spec_inv in SPEC1, SPEC2; simpl in *; now subst.
+    * revert SPEC1 SPEC2.
+      intros (x & SPECi1s & SPEC1s)%ack2_spec_inv_S          (* on a besoin de CE sous-terme strict *)
+             (x' & SPECi2s & SPEC2s)%ack2_spec_inv.          (* mais pas de celui là *)
+      apply ack2_spec_det with (1 := SPEC1s).                (* on utilise le sous-terme SPEC1s the SPEC1 *)
+      now destruct (ack2i_spec_det _ _ _ _ SPECi1s SPECi2s). (* et aussi le sous-terme SPECi1s to SPEC1 *)
+  + destruct n as [ | n ].
+    * apply ack2i_spec_inv in SPECi1, SPECi2; simpl in *; now subst.
+    * exact (ack2_spec_det _ _ _ _ (ack2i_spec_inv_S SPECi1) (ack2i_spec_inv SPECi2)).
+Qed.
+
+(*** On garde ça pour la petite histoire *)
+
+(* L'inversion a une forme "étrange" ici car la valeur de
+   retour doit être un triplet dépendant de la forme
+           exists x, P x /\ Q x 
+   Donc on lui donne la forme d'un principe d'induction *)
+Definition ack_spec_inv_S' (P : nat -> nat -> Prop) m n y : 
+       (forall x, ack2i_spec m n x -> ack2_spec m x y -> P n y)
+    -> ack2_spec (S m) n y -> P n y :=
+  fun f a => match a in ack2_spec m' n' y' return not0 m' -> (forall x : nat, ack2i_spec (pred m') n' x -> ack2_spec (pred m') x y' -> P n' y') -> P n' y' with
+  | ack2_m0 _             => fun C _ => match C with end
+  | ack2_mS m n y (ex_intro _ _ (conj h1 h2)) => fun _ f => f _ h1 h2
+  end I f.
+
